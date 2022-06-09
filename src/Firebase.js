9a22ba787d82598b5bd83214bcd5ca91abdd0f6f
JazibEqbal <jazibeqbal0526@gmail.com>
2022-06-09 23:32:55 +0530

    update

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // import firebase from "firebase";
+import { findByText } from "@testing-library/react";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB61Yqe32KwQMp7Ui-HFLSfqDN-SO0NYSI",
-  authDomain: "clone-cae02.firebaseapp.com",
-  projectId: "clone-cae02",
-  storageBucket: "clone-cae02.appspot.com",
-  messagingSenderId: "336315643115",
-  appId: "1:336315643115:web:1bd53c70b0740c6c2bfaae",
-  measurementId: "G-ET5Y9LSTD9",
-};
+//FIREBASE CONFIG GOES below
+const firebaseConfig=findByText();
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
